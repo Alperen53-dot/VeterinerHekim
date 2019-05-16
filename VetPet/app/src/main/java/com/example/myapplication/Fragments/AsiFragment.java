b2929e0fe89f7b29f56e29b566bf0c78dcb16b13
dumantolga <tolga387@hotmail.com>
2019-05-16 09:11:32 +0300

    Case 26 - Pet aşı takviminin mobile çekilmesi ve takvim üzerinde işaretlenmesi

--- VetPet/app/src/main/java/com/example/myapplication/Fragments/AsiFragment.java
+++ VetPet/app/src/main/java/com/example/myapplication/Fragments/AsiFragment.java
@@ -1,13 +1,18 @@
 package com.example.myapplication.Fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.Models.AsiModel;
 import com.example.myapplication.R;
 import com.example.myapplication.ResApi.ManagerAll;
+import com.example.myapplication.Utils.ChangeFragments;
+import com.squareup.picasso.Picasso;
 import com.squareup.timessquare.CalendarPickerView;
 
 import java.text.DateFormat;
@@ -19,6 +24,7 @@ import java.util.Date;
 import java.util.List;
 
 import androidx.fragment.app.Fragment;
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -42,6 +48,7 @@ public class AsiFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_asi, container, false);
         tanimla();
         getAsi();
+        clickToCalendar();
         return view;
     }
 
@@ -63,21 +70,28 @@ public class AsiFragment extends Fragment {
         req.enqueue(new Callback<List<AsiModel>>() {
             @Override
             public void onResponse(Call<List<AsiModel>> call, Response<List<AsiModel>> response) {
-                if (response.isSuccessful())
-                {
-                    asiList = response.body();
-                    for (int i = 0; i < asiList.size();i++){
-                        String dataString = response.body().get(i).getAsitarih().toString();
-                        try {
-                            Date date = format.parse(dataString);
-                            dataList.add(date);
-                        } catch (ParseException e) {
-                            e.printStackTrace();
+                if (response.isSuccessful()) {
+                    if (response.body().get(0).isTf()) {
+                        asiList = response.body();
+                        for (int i = 0; i < asiList.size(); i++) {
+                            String dataString = response.body().get(i).getAsitarih().toString();
+                            try {
+                                Date date = format.parse(dataString);
+                                dataList.add(date);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                    calendarPickerView.init(today,nextYear.getTime())
-                            .withHighlightedDates(dataList);
+                        calendarPickerView.init(today, nextYear.getTime())
+                                .withHighlightedDates(dataList);
 
+                    }
+                }
+                else
+                {
+                    ChangeFragments changeFragments = new ChangeFragments(getContext());
+                    changeFragments.change(new HomeFragment());
+                    Toast.makeText(getContext(),"PetlerinizeYoktur...",Toast.LENGTH_LONG).show();
                 }
             }
 
@@ -86,6 +100,50 @@ public class AsiFragment extends Fragment {
 
             }
         });
+    }
+    public void clickToCalendar()
+    {
+        calendarPickerView.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                for(int i = 0; i <dataList.size();i++ )
+                {
+                    if (date.toString().equals(dataList.get(i).toString()))
+                    {
+                        openQuestionAlert(asiList.get(i).getPetisim().toString(),asiList.get(i).getAsitarih().toString(),
+                                asiList.get(i).getAsiisim().toString(),asiList.get(i).getPetresim().toString());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onDateUnselected(Date date) {
+
+            }
+        });
+    }
+    public void openQuestionAlert(String petIsmi,String tarih, String asiIsmi,String resimUrl){
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.asitakiplayout,null);
+
+        TextView petİsimText = (TextView) view.findViewById(R.id.petİsimText);
+        TextView petAsiTakipBilgiText = (TextView) view.findViewById(R.id.petAsiTakipBilgiText);
+        CircleImageView asiTakipCircleImageView = (CircleImageView) view.findViewById(R.id.asiTakipCircleImageView);
+
+        petİsimText.setText(petIsmi);
+        petAsiTakipBilgiText.setText(petIsmi + " isimli petinizin " + tarih + " tarihinde " + asiIsmi + "aşısı yapılacaktır");
+
+        Picasso.get().load(resimUrl).into(asiTakipCircleImageView);
+
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setView(view);
+        alert.setCancelable(true); //boş bir yere tıklayınca kapat
+
+        final AlertDialog alertDialog = alert.create();
+
+        alert.show();
     }
 
 
--- VetPet/app/src/main/java/com/example/myapplication/ResApi/BaseUrl.java
+++ VetPet/app/src/main/java/com/example/myapplication/ResApi/BaseUrl.java
@@ -1,5 +1,5 @@
 package com.example.myapplication.ResApi;
 
 public class BaseUrl{
-    public static final String URL = "http://192.168.1.104/";
+    public static final String URL = "http://192.168.1.106/";
 }
